3b5f0ff568cecf1aafed5b627d17b8861bdc408f
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-04-04 23:43:16 +0200

    Event listener function
    Corrected the event listeners as they were added incorrectly.
    
    Added an event listener in order to prevent the modal from closing.

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ function addTaskToUI(task) {
 function setupEventListeners() {
   // Cancel editing task event listener
   const cancelEditBtn = document.getElementById('cancel-edit-btn');
-  cancelEditBtn.click() => toggleModal(false, elements.editTaskModal));
+  cancelEditBtn.addEventListener("click", () => toggleModal(false, elements.editTaskModal));
 
   // Cancel adding new task event listener
   const cancelAddTaskBtn = document.getElementById('cancel-add-task-btn');
@@ -227,9 +227,14 @@ function setupEventListeners() {
     elements.filterDiv.style.display = 'none'; // Also hide the filter overlay
   });
 
+  //Prevent modal clicks from closing it
+  elements.modalWindow.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent the click event from bubbling up to the filterDiv
+  });
+
   // Show sidebar event listener
-  elements.hideSideBarBtn.click() => toggleSidebar(false));
-  elements.showSideBarBtn.click() => toggleSidebar(true));
+  elements.hideSideBarBtn.addEvenetListener("click", () => toggleSidebar(false));
+  elements.showSideBarBtn.addEventListener("click", () => toggleSidebar(true));
 
   // Theme switch event listener
   elements.themeSwitch.addEventListener('change', toggleTheme);
